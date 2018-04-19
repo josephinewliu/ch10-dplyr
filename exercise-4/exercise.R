46c0e8e3966f77ac0ef002131958bc19655e72b1
Josephine <josephinewliu@gmail.com>
2018-04-18 17:50:33 -0700

    followed along in class and edited from solution branch

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -14,19 +14,17 @@ library(dplyr)
 View(flights)
 nrow(flights)
 ncol(flights)
-colname(flights)
-??flights
+colnames(flights)
+?flights
 
 # Use `dplyr` to give the data frame a new column that is the amount of time
 # gained or lost while flying (that is: how much of the delay arriving occured
 # during flight, as opposed to before departing).
-flights <- flights %>%
-  mutate(time_gained = arr_delay - dep_delay) 
+flights <- mutate(flights, time_gained = arr_delay - dep_delay) 
 
 # Use `dplyr` to sort your data frame in descending order by the column you just
 # created. Remember to save this as a variable (or in the same one!)
-flights <- flights %>% 
-  arrange(time_gained)
+flights <- arrange(flights, desc(time_gained))
 
 # For practice, repeat the last 2 steps in a single statement using the pipe
 # operator. You can clear your environmental variables to "reset" the data frame
@@ -35,10 +33,11 @@ flights <- flights %>%
   arrange(-time_gained)
 
 # Make a histogram of the amount of time gained using the `hist()` function
+hist(flights$time_gained)
 
 # On average, did flights gain or lose time?
 # Note: use the `na.rm = TRUE` argument to remove NA values from your aggregation
-summarize(flights, mean = mean(flights$time_gained, na.rm = TRUE))
+mean(flights$time_gained, na.rm = TRUE)
 
 # Create a data.frame of flights headed to SeaTac ('SEA'), only including the
 # origin, destination, and the "gain_in_air" column you just created
@@ -47,15 +46,14 @@ to_sea <- flights %>%
   select(origin, dest, time_gained)
 
 # On average, did flights to SeaTac gain or loose time?
-summarize(jfk_sea, mean = mean(time_gained, na.rm = TRUE))
+mean(to_sea$time_gained, na.rm = TRUE)
 
 # Consider flights from JFK to SEA. What was the average, min, and max air time
 # of those flights? Bonus: use pipes to answer this question in one statement
 # (without showing any other data)!
-summary <- flights %>% 
-  filter(origin == "JFK", dest == "SEA") %>% 
+filter(flights, origin == "JFK", dest == "SEA") %>% 
   summarize(
-    avg_time = mean(time_gained, na.rm = TRUE)
-    min_time = min(time_gained, na.rm = TRUE)
-    max_time = max(time_gained, na.rm = TRUE)
+    avg_air_time = mean(air_time, na.rm = TRUE),
+    max_air_time = max(air_time, na.rm = TRUE),
+    min_air_time = min(air_time, na.rm = TRUE)
   )
